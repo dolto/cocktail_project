5fb98d0094a971a8ee9aa5d46df15307708048f1
sehoon0302 <saehun66@gmail.com>
2023-10-13 10:44:41 +0900

    어바웃어스 이미지 6개 추가

--- src/Component/Main/AboutUs.tsx
+++ src/Component/Main/AboutUs.tsx
@@ -1,7 +1,32 @@
+import img1 from "../res/img/Aboutus-1.jpg";
+import img2 from "../res/img/Aboutus-2.jpg";
+import img3 from "../res/img/Aboutus-3.jpg";
+import img4 from "../res/img/Aboutus-4.jpeg";
+import img5 from "../res/img/Aboutus-5.jpeg";
+import "../Css/AboutUs.css";
+
 const AboutUs = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div>
+      <div>
+        <div className="imgcontainer">
+          <img className="img1" src={img1} alt="About Us" />
+        </div>
+        <div className="imgcontainer">
+          <img className="img2" src={img2} alt="About Us" />
+        </div>
+        <div className="imgcontainer">
+          <img className="img3" src={img3} alt="About Us" />
+        </div>
+        <div className="imgcontainer">
+          <img className="img4" src={img4} alt="About Us" />
+        </div>
+        <div className="imgcontainer">
+          <img className="img5" src={img5} alt="About Us" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
