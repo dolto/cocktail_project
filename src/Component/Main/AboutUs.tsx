bbe32b7622981be717644c445ce0b94f33feff6a
sehoon0302 <saehun66@gmail.com>
2023-10-15 04:03:40 +0900

    어바웃어스 페이지 기초 완성 (폰트수정, 풀페이지 스크롤 추가예정)

--- src/Component/Main/AboutUs.tsx
+++ src/Component/Main/AboutUs.tsx
@@ -1,31 +1,108 @@
-import img1 from "../res/img/Aboutus-1.jpg";
-import img2 from "../res/img/Aboutus-2.jpg";
-import img3 from "../res/img/Aboutus-3.jpg";
-import img4 from "../res/img/Aboutus-4.jpeg";
-import img5 from "../res/img/Aboutus-5.jpeg";
-import "../Css/AboutUs.css";
+import React, { useEffect, useState } from "react"; // React 및 useState, useEffect를 import해야 합니다
+import img1 from "../../res/img/Aboutus-1.jpg";
+import img2 from "../../res/img/Aboutus-2.jpg";
+import img3 from "../../res/img/Aboutus-3.jpg";
+import img4 from "../../res/img/Aboutus-4.jpeg";
+import img5 from "../../res/img/Aboutus-5.jpeg";
+import img6 from "../../res/img/Aboutus-6.jpeg";
+//import "../Css/AboutUs.css";
+import AboutUsPage from "../../styled/AboutUs";
 
 const AboutUs = () => {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const imgStyle = {
+    height: windowHeight + "px", // 이미지 높이를 브라우저 높이에 맞게 설정
+  };
+
   return (
-    <div>
-      <div>
-        <div className="imgcontainer">
-          <img className="img1" src={img1} alt="About Us" />
+    <AboutUsPage>
+        <div>
+          <div className="section1">
+            <div className="imgcontainer">
+              <img className="img1" src={img1} alt="About Us" style={imgStyle} />
+            </div>
+            <p>
+              🍸 칵테일 레시피
+              <br />
+              🍾 주류 구매
+              <br />
+              📝 원데이 클래스까지
+            </p>
+            <h1>한번에 만나보세요!</h1>
+          </div>
+          <div className="section2">
+            <div className="imgcontainer">
+              <img className="img2" src={img2} alt="About Us" style={imgStyle} />
+            </div>
+            <p>
+              고생한 나를 좋은 술로 위로하고 싶을 때<br />
+              친구, 연인과 함께 분위기를 내고 싶을때
+              <br />
+              특별한 날을 완성시키는 방법
+            </p>
+          </div>
+          <div className="section3">
+            <div className="imgcontainer">
+              <img className="img3" src={img3} alt="About Us" style={imgStyle} />
+            </div>
+            <p>
+              600여가지가 넘는
+              <br />
+              칵테일 레시피로
+              <br />
+              맛있는 주류여행을
+              <br />
+              떠나보세요!
+            </p>
+          </div>
+          <div className="section4">
+            <div className="imgcontainer">
+              <img className="img4" src={img4} alt="About Us" style={imgStyle} />
+            </div>
+            <p>
+              홈술을 위해 필요한
+              <br />
+              주류, 쉐이커, 술잔 구매까지 모두
+              <br />
+              Cocktail Sense에서!
+            </p>
+          </div>
+          <div className="section5">
+            <div className="imgcontainer">
+              <img className="img5" src={img5} alt="About Us" style={imgStyle} />
+            </div>
+            <p>
+              매번 모집글을 직접 찾고
+              <br />
+              신청따로 결제따로 해야 했던 <br />
+              원데이 클래스
+              <br />
+              이제는 검색, 신청, 결제까지 한번에 하세요
+              <br />
+            </p>
+          </div>
+          <div className="section6">
+            <div className="imgcontainer">
+              <img className="img6" src={img6} alt="About Us" style={imgStyle} />
+            </div>
+            <p>Anyone can easily</p>
+            <h1>Cocktail sense</h1>
+          </div>
         </div>
-        <div className="imgcontainer">
-          <img className="img2" src={img2} alt="About Us" />
-        </div>
-        <div className="imgcontainer">
-          <img className="img3" src={img3} alt="About Us" />
-        </div>
-        <div className="imgcontainer">
-          <img className="img4" src={img4} alt="About Us" />
-        </div>
-        <div className="imgcontainer">
-          <img className="img5" src={img5} alt="About Us" />
-        </div>
-      </div>
-    </div>
+    </AboutUsPage>
   );
 };
 
---
+++ src/styled/AboutUs.tsx
@@ -0,0 +1,136 @@
+import styled from "styled-components";
+import Sinoreta_PERSONAL_USE_ONLY from "../res/font/Sinoreta_PERSONAL_USE_ONLY.woff";
+import AboutUs from "../Component/Main/AboutUs";
+
+const AboutUsPage = styled.header`
+  @font-face {
+  font-family: "Sinoreta_PERSONAL_USE_ONLY";
+  src: url(${Sinoreta_PERSONAL_USE_ONLY}) format("woff");
+}
+
+/* AboutUs.css */
+.section1{
+  position: relative;
+}
+.section1 p {
+  display: flex;
+  position: absolute;
+  top: 20%;
+  left: 20%;
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  font-size: 3.5rem;
+}
+.section1 h1 {
+  position: absolute;
+  top: 55%;
+  left: 20%;
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  font-size: 6rem;
+}
+.section2 {
+  position: relative;
+}
+.section2 p {
+  text-align: center;
+  line-height: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  font-size: 3rem;
+}
+.section3 {
+  position: relative;
+}
+.section3 p {
+  text-align: right;
+  line-height: 100px;
+  position: absolute;
+  top: 50%;
+  right: 30%;
+  transform: translate(50%, -50%);
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  font-size: 3rem;
+}
+.section4 {
+  position: relative;
+}
+.section4 p {
+  text-align: center;
+  line-height: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  text-shadow: 4px 4px 4px black;
+  font-size: 3rem;
+}
+.section5 {
+  position: relative;
+}
+.section5 p {
+  text-align: center;
+  line-height: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  text-shadow: 4px 4px 4px black;
+  font-size: 3rem;
+}
+.section6 {
+  position: relative;
+}
+.section6 p {
+  text-align: center;
+  line-height: 100px;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  text-shadow: 2px 4px 2px black;
+  font-size: 2rem;
+}
+.section6 h1 {
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white; /* 텍스트의 색상을 조절할 수 있습니다 */
+  text-shadow: 4px 4px 2px black;
+  font-size: 6rem;
+}
+
+
+
+.imgcontainer {
+  position: relative;
+  width: 100%;
+  text-align: center;
+}
+.imgcontainer::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4); /* 20% 투명도의 검은색 레이어 */
+}
+.imgcontainer img {
+  width: 100%;
+  height: auto;
+  display: block;
+  margin: 0 auto;
+}
+.img3 {
+  height: 100vh; /* 이미지 높이를 브라우저 창의 높이에 맞게 조절 */
+  width: auto; /* 이미지 너비를 자동으로 조절하여 비율 유지 */
+}
+`;
+
+export default AboutUsPage;
