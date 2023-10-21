5743ae12200937716ca89097bf65eb4adb6137ba
dolto <hdw810@naver.com>
2023-10-21 11:28:25 +0900

    진짜 최종

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/cocktail_sense'>
       <RecoilRoot>
         <App />
       </RecoilRoot>
